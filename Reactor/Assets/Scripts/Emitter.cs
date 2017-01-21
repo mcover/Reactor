ceca12b79fa405074d1038aba657154606301fc0
mcover <mcover@mit.edu>
2017-01-20 22:59:24 -0500

    Game Scripts and Starter Emitter
    Added basic game scripts and starter emitter prefab

---
+++ Reactor/Assets/Scripts/Emitter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+    public int frequency;
+    public float time;
+    public int width;
+    public bool activated = false;
+    public float direction;
+    public AudioSource audioSource;
+    public bool movable;
+    public float waveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (activated) {
+            //check time
+            //if frequency time has passed shoot wave of width in direction
+        }
+	}
+
+    void sendWave(){
+        //create wave object from prefab of width
+        //add movement script with speed and direction
+    }
+}
