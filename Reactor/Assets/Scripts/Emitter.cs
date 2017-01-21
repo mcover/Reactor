e46d07b8f13da3a927e50de3b6d7a5c2426284d7
mcover <mcover@mit.edu>
2017-01-21 16:49:11 -0500

    Edits to emitter script

--- Reactor/Assets/Scripts/Emitter.cs
+++ Reactor/Assets/Scripts/Emitter.cs
@@ -3,29 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Emitter : MonoBehaviour {
-    public float frequency;
-    private float time;
-    public int width;
+   
     public bool activated = false;
-    public Quaternion direction;
     public AudioSource audioSource;
     public bool movable;
-    public float waveSpeed;
     public bool source;
-    public float distance = 100;
-    public GameObject Wave30;
-    public GameObject Wave360;
-
+    public float frequency;
+    public float time;
+   
 	// Use this for initialization
 	void Start () {
-        time = Time.time;
+      
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float newTime = Time.time;
         //print(source & activated & ((newTime - time) >= frequency));
-		if (source & activated & ((newTime - time) >=frequency)) {
+		if (source & activated) {//need to check time vs frequency as well
             //shoot wave (could be wave of distance 0)
            // print("making wave");
             //need offset
@@ -52,24 +47,9 @@ public class Emitter : MonoBehaviour {
             activated = !activated;
         }
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    public void TriggerEmitter()
     {
-        if (!source)
-        {
-            //need an offset
-            //send a wave
-           // if (width == 30)
-            //{
-             //   GameObject newWave = Instantiate(Wave30, transform.position, direction) as GameObject;
-              //  newWave.GetComponent<Wave>().SetProperties(distance, 5, direction, waveSpeed);
-           // }
-           // else if (width == 360)
-            //{
-             //   GameObject newWave = Instantiate(Wave360, transform.position, direction) as GameObject;
-              //  newWave.GetComponent<Wave>().SetProperties(distance,5,direction,waveSpeed);
-           // }
-        }
         //play sound
+        //activate circle script in a corroutine
     }
-
 }
