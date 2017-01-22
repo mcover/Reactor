f593d2309b23fb7d4f70dbfb5188dc6940bcd10d
mcover <mcover@mit.edu>
2017-01-21 23:17:56 -0500

    Added starter ui

---
+++ Reactor/Assets/Scripts/NewEmitterSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEmitterSpawner : MonoBehaviour {
+    public GameObject emitterPrefab;
+    public Transform spawnPosition;
+	
+    public void SpawnNewEmitter()
+    {
+        GameObject newPiece = Instantiate(emitterPrefab, spawnPosition.position, Quaternion.identity) as GameObject;
+        Debug.Log("Being called");
+    }
+}
