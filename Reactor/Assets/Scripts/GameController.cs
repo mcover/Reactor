2b989ffc8d6036c36c9f42dc40afe2360c4e0052
mcover <mcover@mit.edu>
2017-04-01 16:39:37 -0400

    Added toolbox and added more goal code to game controller

--- Reactor/Assets/Scripts/GameController.cs
+++ Reactor/Assets/Scripts/GameController.cs
@@ -7,6 +7,27 @@ public class GameController : MonoBehaviour {
     public string goalType;
     public int goalNum;
 
+    private int goalProgress;
 
+    private bool CheckGoal()
+    {
+        if(goalType == "targetHit")
+        {
+            if (goalProgress>= goalNum)
+            {
+                return true;
+            }   
+        }
+        else if (goalType == "stopWaves")
+        {
+            if(goalProgress == goalNum)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //need to set up action listeners for updating the goal values
 
 }
---
+++ Reactor/Assets/Scripts/ToolBox.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Toolbox
+{
+    private static Dictionary<Type, object> _tools = new Dictionary<Type, object>();
+
+    /// <summary>
+    /// Registers the Tool. Use on Awake only.
+    /// </summary>
+    /// <typeparam name="T">The tool's type.</typeparam>
+    /// <param name="tool">The tool to register.</param>
+    /// <returns>False of Tool of Type T already exists, true otherwise.</returns>
+    public static bool RegisterAsTool<T>(T tool)
+    {
+        if (GetTool<T>() != null)
+            return false;
+
+        _tools.Add(typeof(T), tool);
+        return true;
+    }
+
+    /// <summary>
+    /// Gets the Tool of type T. Use on Start only.
+    /// </summary>
+    /// <typeparam name="T">The Type of the tool.</typeparam>
+    /// <returns>Object of Type T found in Toolbox, otherwise default(T)</returns>
+    public static T GetTool<T>()
+    {
+        object tool;
+
+        if (_tools.TryGetValue(typeof(T), out tool))
+        {
+            return (T)tool;
+        }
+
+        return default(T);
+    }
+
+    public static void UnregisterTool<T>(T tool)
+    {
+        _tools.Remove(typeof(T));
+    }
+
+    public static void ResetToolBox()
+    {
+        _tools = new Dictionary<Type, object>();
+    }
+}
+
